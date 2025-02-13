2b4f6a8f73664fb95e1939b6798dac849fc88fac
Simon-Cheek <simon.cheek@yahoo.com>
2025-02-13 08:39:05 -0700

    Add userservice to logout

--- tweeter/tweeter-web/src/components/appNavbar/AppNavbar.tsx
+++ tweeter/tweeter-web/src/components/appNavbar/AppNavbar.tsx
@@ -5,6 +5,7 @@ import Image from "react-bootstrap/Image";
 import { AuthToken } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "../userInfo/UserInfoHook";
+import { UserService } from "../../model/service/UserService";
 
 const AppNavbar = () => {
   const location = useLocation();
@@ -12,11 +13,13 @@ const AppNavbar = () => {
   const { displayInfoMessage, displayErrorMessage, clearLastInfoMessage } =
     useToastListener();
 
+  const userService = new UserService();
+
   const logOut = async () => {
     displayInfoMessage("Logging Out...", 0);
 
     try {
-      await logout(authToken!);
+      await userService.logout(authToken!);
 
       clearLastInfoMessage();
       clearUserInfo();
@@ -25,11 +28,6 @@ const AppNavbar = () => {
         `Failed to log user out because of exception: ${error}`
       );
     }
-  };
-
-  const logout = async (authToken: AuthToken): Promise<void> => {
-    // Pause so we can see the logging out message. Delete when the call to the server is implemented.
-    await new Promise((res) => setTimeout(res, 1000));
   };
 
   return (
--- tweeter/tweeter-web/src/model/service/UserService.ts
+++ tweeter/tweeter-web/src/model/service/UserService.ts
@@ -2,6 +2,11 @@ import { Buffer } from "buffer";
 import { AuthToken, FakeData, User } from "tweeter-shared";
 
 export class UserService {
+  public async logout(authToken: AuthToken): Promise<void> {
+    // Pause so we can see the logging out message. Delete when the call to the server is implemented.
+    await new Promise((res) => setTimeout(res, 1000));
+  }
+
   public async getUser(
     authToken: AuthToken,
     alias: string
